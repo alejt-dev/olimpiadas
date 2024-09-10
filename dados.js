43922a0581a8239593835ff7ac3d3e339c5d0603
alejt-dev <alessandra.jt@hotmail.com>
2024-09-09 21:26:06 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,24 @@
+// Declaração de variável que irá armazenar as informações dos atletas criando uma lista (conceito: armazenar mais de uma inofrmação em uma variavel) com objetos
+let dados = [
+  {
+    titulo: "Rayssa Leal",
+    descricao:
+      "Rayssa Leal, conhecida como Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento precoce. Aos poucos anos, ela já coleciona títulos e se tornou um ícone da nova geração do skate. Rayssa é um exemplo de que a idade não é limite para alcançar o sucesso.",
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags: "skate fada",
+  },
+  {
+    titulo: "Beatriz Souza",
+    descricao:
+      "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de 2024, conquistando a medalha de ouro. Sua técnica e determinação a tornaram uma das principais atletas do judô brasileiro.",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+    tags: "judoca judô",
+  },
+  {
+    titulo: "Rebeca Andrade",
+    descricao:
+      "Rebeca Andrade é uma ginasta artística brasileira que fez história nos Jogos Olímpicos de 2020 e 2024, conquistando diversas medalhas. Sua versatilidade e força a tornaram um ícone do esporte brasileiro.",
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    tags: "ginasta artística",
+  },
+];
